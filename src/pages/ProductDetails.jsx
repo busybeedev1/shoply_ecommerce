bbdbf2b8429d5c30f4d4a2855abfa6f2d4a48f28
busybeedev <ebesuvictor@gmail.com>
2026-07-16 15:21:05 +0100

    Fix API service imports and deployment paths

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { getProduct } from "../../../servicesapi";
+import { getProduct } from "../servicesapi";
 import "./productDetails.css";
 
 import React from "react";
--- src/servicesapi.js
+++ src/servicesapi.js
@@ -1,5 +1,31 @@
-const BASE_URL = "";
+const BASE_URL = "https://dummyjson.com";
+
 
 export async function getProducts() {
-    return [];
+    const response = await fetch(`${BASE_URL}/products`);
+    return response.json();
+}
+
+
+export async function getProduct(id) {
+    const response = await fetch(`${BASE_URL}/products/${id}`);
+    return response.json();
+}
+
+
+export async function searchProducts(query) {
+    const response = await fetch(
+        `${BASE_URL}/products/search?q=${query}`
+    );
+
+    return response.json();
+}
+
+
+export async function getCategories() {
+    const response = await fetch(
+        `${BASE_URL}/products/categories`
+    );
+
+    return response.json();
 }
