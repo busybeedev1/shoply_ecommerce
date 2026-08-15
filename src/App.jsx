8f5577b84cdd4dc1f7a2fa30beeb525928dd025f
busybeedev <ebesuvictor@gmail.com>
2026-08-15 02:16:57 +0100

    updated navbar/scrolling banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import ProductDetails from "./pages/ProductDetails";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import ScrollingBanner from "./components/ScrollingBanner";
 import Footer from "./components/Footer";
-import HeroSection from "./components/HeroSection";
+
 // import Cart from "./pages/Cart";
 // import Search from "./pages/Search";
 
@@ -13,22 +14,23 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <ScrollingBanner />
-      <Routes>
 
-        <Route path="/" element={<Home />} />
+      <main className="app-main">
+        <ScrollingBanner />
 
-        <Route
-          path="/product/:id"
-          element={<ProductDetails />}
-        />
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-        {/* <Route path="/cart" element={<Cart />} /> */}
-        {/* <Route path="/search" element={<Search />} /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
 
-      </Routes>
-      <ScrollingBanner />
+          <Route
+            path="/product/:id"
+            element={<ProductDetails />}
+          />
+
+          {/* <Route path="/cart" element={<Cart />} /> */}
+          {/* <Route path="/search" element={<Search />} /> */}
+        </Routes>
+      </main>
+
       <Footer />
     </BrowserRouter>
   );
